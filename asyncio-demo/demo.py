de7d9a5db5b81e0e5a9666951847bdece8f9342a
DerekDuan <duan.shiqi@gmail.com>
2024-04-11 15:10:46 +0800

    Add asyncio demo

---
+++ asyncio-demo/demo.py
@@ -0,0 +1,64 @@
+import asyncio
+
+
+# async def main():
+#     print("Hello ...")
+#     await asyncio.sleep(1)
+#     print("... World!")
+
+
+# asyncio.run(main())
+
+
+# async def count():
+#     print("One")
+#     await asyncio.sleep(3)
+#     print("Two")
+
+
+# async def main():
+#     await asyncio.gather(count(), count(), count())
+
+
+# if __name__ == "__main__":
+#     import time
+
+#     s = time.perf_counter()
+#     asyncio.run(main())
+#     elapsed = time.perf_counter() - s
+#     print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+
+
+import asyncio
+import random
+
+# ANSI colors
+c = (
+    "\033[0m",  # End of color
+    "\033[36m",  # Cyan
+    "\033[91m",  # Red
+    "\033[35m",  # Magenta
+)
+
+
+async def makerandom(idx: int, threshold: int = 6) -> int:
+    print(c[idx + 1] + f"Initiated makerandom({idx}).")
+    i = random.randint(0, 10)
+    while i <= threshold:
+        print(c[idx + 1] + f"makerandom({idx}) == {i} too low; retrying.")
+        await asyncio.sleep(idx + 1)
+        i = random.randint(0, 10)
+    print(c[idx + 1] + f"---> Finished: makerandom({idx}) == {i}" + c[0])
+    return i
+
+
+async def main():
+    res = await asyncio.gather(*(makerandom(i, 10 - i - 1) for i in range(3)))
+    return res
+
+
+if __name__ == "__main__":
+    random.seed(444)
+    r1, r2, r3 = asyncio.run(main())
+    print()
+    print(f"r1: {r1}, r2: {r2}, r3: {r3}")
